8a7042da9cea1dbc3f177f8639b0a3935046d041
yaelm002Pro <yassin.fpn@gmail.com>
2021-02-25 16:44:27 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+document.getElementById("btn").addEventListener("click", getJokes);
+
+
+function getJokes(e){
+  var zahl = document.querySelector("#number").value;
+  console.log(zahl);
+  const xhr = new XMLHttpRequest;
+
+  xhr.open("GET", `http://api.icndb.com/jokes/random/${zahl}`, true);
+
+  xhr.onload = function(){
+      if (this.status ===200){
+    let erg = JSON.parse(this.responseText);
+    let output ="";
+    let i = 0;
+    if(erg.type=== "success"){
+    erg.value.forEach(function(joke){
+        output += `<img src ="${++i}.jpeg" alt ="no bild" width ="40" height ="40"> `;
+        output += `<li>${joke.id}</li>`;
+        output += `<li>${joke.joke}</li>`;
+    });
+  } else {
+    output += `somthing goes wrong`;
+  }
+
+
+    document.getElementById("list").innerHTML = output;
+        
+  }
+
+}
+
+
+
+  xhr.send();
+  
+  e.preventDefault();
+
+}
